4b81be6ed3c8c3a3d3f88d26fb65778e403972ab
W1ndys <w1ndys@outlook.com>
2025-03-14 11:58:29 +0800

    chore: update base URL in site configuration to reflect new deployment path

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/QFNUCyberWiki/",
   lang: "zh-CN",
   title: "QFNUCyberwiki",
   description:
