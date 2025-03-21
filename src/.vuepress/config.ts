1c1e3605a480503d8bfd41d64780563ed8ce41d2
W1ndys <w1ndys@outlook.com>
2025-03-21 14:50:30 +0800

    chore: remove slimsearch plugin and clean up theme configuration

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,4 @@
 import { defineUserConfig } from "vuepress";
-import { slimsearchPlugin } from '@vuepress/plugin-slimsearch'
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -13,10 +12,6 @@ export default defineUserConfig({
   head: [
   ],
 
-  plugins: [
-    slimsearchPlugin({
-    }),
-  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,5 +1,4 @@
 import { hopeTheme } from "vuepress-theme-hope";
-
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
